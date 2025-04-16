e456cd41c90dc168dc6dd8cf4a1f327318075dfa
raphaeldamevin <72107589+damevin@users.noreply.github.com>
2025-04-16 16:11:17 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import CopyIssue from './components/CopyIssue.vue';
+</script>
+
+<template>
+  <div>
+    <h1>Playground Clipboard Issue</h1>
+  <CopyIssue></CopyIssue>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
